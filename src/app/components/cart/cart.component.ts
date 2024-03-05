37d70285223b733e766e84acf1424778018019cf
Ayala-k <kluftayala@gmail.com>
2024-03-05 17:37:57 +0200

    manage cart rerendering after adding item or creating order

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import ICartItem from '../../models/cartItem';
+import { Subscription } from 'rxjs';
+import { MainService } from '../../services/main.service';
 
 @Component({
   selector: 'app-cart',
@@ -11,36 +13,42 @@ import ICartItem from '../../models/cartItem';
 export class CartComponent implements OnInit {
   cartItems!: ICartItem[];
   totalPrice: number = 0;
+  private cartSubscription!: Subscription;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,private mainService:MainService) { }
 
   ngOnInit(): void {
     console.log("cart init");
+    this.getCart();
+    this.cartSubscription = this.mainService.cartUpdated$.subscribe(() => {
+      console.log("cart update event");
+      this.getCart();
+      
+    });
+  }
 
+
+  getCart(){
     this.cartService.getCart().subscribe(
       (response: any) => {
-        this.cartItems = response;
-        this.calculateTotalPrice()
-
+        this.cartItems = response.items;
+        this.totalPrice=response.totalPrice;
       },
       error => {
         console.log(error.error);
+        if(error.status==404){
+          this.cartItems=[]
+          this.totalPrice=0;
+        }
       }
     );
-  }
-
-  calculateTotalPrice() {
-    let sum = 0;
-    this.cartItems.forEach(item => {
-      sum += item.amount * item.price;
-    })
-    this.totalPrice = sum
   }
 
   createOrder(){
     this.cartService.createOrder().subscribe(
       (response: any) => {
         console.log(response);
+        this.mainService.updateCart();
       },
       error => {
         console.log(error.error);
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import IItem from '../../models/item';
 import { CartService } from '../../services/cart.service';
+import { MainService } from '../../services/main.service';
 
 @Component({
   selector: 'app-item',
@@ -11,20 +12,17 @@ export class ItemComponent {
   @Input() item!: IItem;
   errorMessage!: string;
 
-
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private mainService:MainService) { }
 
   addToCart(itemId: number) {
-    console.log("adding",itemId);
     this.cartService.addToCart(itemId).subscribe(
       (response: any) => {
-        console.log("after req");
+        this.mainService.updateCart();
       },
       error => {
-        console.log("errorrrrr",error);
-        
+        console.log(error);
         this.errorMessage = error.error || 'An error occurred while logging in.';
       }
-    ); 
+    );
   }
 }
